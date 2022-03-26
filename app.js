b714076976e0e07749ea03d1a10fb423430ebdb9
Trent Loyd <trent.loyd@gmail.com>
2022-03-26 18:05:08 -0400

    adding header/footer pages

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 var items = [];
+var workItems = [];
 
 const weekday = [
   "Sunday",
@@ -21,35 +22,46 @@ const d = new Date();
 
 app.set("view engine", "ejs");
 
+//Set Day
+var today = new Date();
+
+var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+};
+
+var day = today.toLocaleDateString("en-US", options);
+
 app.get("/", function (req, res) {
-  var today = new Date();
-
-  var options = {
-      weekday: "long",
-      day: "numeric",
-      month: "long"
-  };
-
-  var day = today.toLocaleDateString("en-US", options);
-
-  //   var currentDay = today.getDay();
-  //   var day = "";
-
-  // if (currentDay === 6 || currentDay === 0){
-  //     day = "Weekend";
-  // } else{
-  //     day = "Weekday";
-  // }
-  //   day = weekday[d.getDay()];
-
-  res.render("list", { kindOfDay: day, newListItem: items });
+  res.render("list", { listTitle: day, newListItem: items });
 });
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;   
+  //console.log(req.body);
+
+  var item = req.body.newItem;   
+  
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  } else{
     items.push(item);
     res.redirect("/");
-})
+  }
+
+});
+
+app.get("/work", function(req,res){
+  res.render("list",  { listTitle: "Work List", newListItem: workItems });
+});
+
+// app.post("/work", function(req,res){
+//   let item = req.body.newItem;
+//   workItems.push(item);
+//   res.redirect("/work");
+// });
+
 
 app.listen(3000, function () {
   console.log("Server started on port 3000 local");
